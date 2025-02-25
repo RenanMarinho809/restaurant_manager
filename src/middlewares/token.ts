6a8263705a70eb853032764507ebac1fb2ef1f89
RenanMarinho809 <renanmarinho809@gmail.com>
2025-02-25 18:02:52 -0300

    feat: implement user creation and JWT token generation/verification

--- src/middlewares/token.ts
+++ src/middlewares/token.ts
@@ -1,8 +1,23 @@
 //Gerar token ao fazer login
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import { User } from '../models/validation';
 
 dotenv.config();
 
 const secret = process.env.HASHPASSWORD as string;
 
+export const generateToken = (user: User) => {
+    const token = jwt.sign({ user: user.email , role: user.role}, secret, {
+        expiresIn: '1h'
+    })
+}
+
+export const verifyToken = (token: string) => {
+    try {
+        const decoded = jwt.verify(token, secret);
+        return decoded;
+    } catch (error) {
+        return null;
+    }
+}
--- src/models/validation.ts
+++ src/models/validation.ts
@@ -3,4 +3,29 @@ export interface User {
     role: string;
   }
 
- 
+export interface Usuario {
+  id: number;
+  nome: String;
+  email: String;
+  senha: String;
+  role: String;
+  reservas: number;
+}
+
+export interface Mesa {
+  id: number;
+  nome: String;
+  capacidade: number;
+  status: String;
+  reservas: number;
+}
+
+export interface Reserva {
+  id           : number;
+  usuario_id :  number;
+  mesa_id   :   number;
+  data_reserva : String;
+  status    :   String;
+  usuario   :  number;
+  mesa      :   number;
+}
